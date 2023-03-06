eb2d3c0e0f5fe78f5eccc831a4bf21fd5dae32b0
Fabio Pilon <100612310+fabiop87@users.noreply.github.com>
2023-03-06 15:06:18 -0300

    Create CRUDtodotorto.php

---
+++ AnotaçõesPHP/CRUDtodotorto.php
@@ -0,0 +1,43 @@
+<?php 
+
+require_once '../Config.php';
+// Conexao
+
+$pdo =  new PDO(
+  "mysql:host=" . MYSQL_HOST . ";" . 
+  "dbname=" . MYSQL_DATABASE . ";charset=utf8",
+  MYSQL_USER,
+  MYSQL_PASS
+);
+
+//Sem parametrização
+
+//Select
+$clientes = $pdo->query("SELECT * FROM clientes")->fetchAll(PDO::FETCH_ASSOC);
+echo <pre>;
+print_r($clientes);
+
+//Insert
+$pdo->query("INSERT INTO clientes VALUES(0, 'awawa')");
+
+//Update
+$pdo-<query("UPDATE clientes SET nome = 'bananinha' WHERE id_cliente = 3");
+
+//Delete
+$pdo->query("DELETE FROM clientes WHEREE id_cliente = 3");
+
+//Melhor
+$nome_cliente = 'jhonson';
+
+// $q = $pdo->query("INSERT INTO clientes VALUES(0, ?)");
+// $q->execute([$nome_cliente]);
+//
+
+$q = $pdo->query("INSERT INTO clientes VALUES(0, :nome)");
+$q->execute(
+  [
+    'nome' => $nome_cliente
+  ]
+);
+
+?>
